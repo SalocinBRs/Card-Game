13b566b6bf7c36fcbcc77f8e3f4c6648612cf33c
SalocinBRs <nv15983@gmail.com>
2023-11-17 12:18:03 -0300

    v0.3

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -177,6 +177,9 @@ async function playAudio(status) {
 function init() {
     drawCards(5, playerSides.player1)
     drawCards(5, playerSides.computer)
+
+    const bgm  = document.getElementById("bgm")
+    bgm.play()
 }
 
 init()
